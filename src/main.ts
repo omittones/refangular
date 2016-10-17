87deaa6e8e2b2775b76922705b1b7e92a387bbad
Augustin Juricic <ajuricic@neogov.net>
2016-10-17 17:26:31 +0200

    Remove hardcoded dirs.

--- src/main.ts
+++ src/main.ts
@@ -27,57 +27,104 @@ function recurse(file: string, include?: (s: string) => boolean): string[] {
     return output;
 }
 
+function checkProject(root: string) {
+    var checkPaths = ['src/WebApp', 'src/WebApp/scripts/app'];
+    var absPaths = checkPaths.map(p => {
+        return mpath.resolve(mpath.join(root, p));
+    });
+    try {
+        absPaths.forEach(p => mfs.accessSync(p));
+        return true;
+    }
+    catch (err) {
+        return false;
+    }
+}
+
 function main() {
 
-    var sourceManager = new SourceFileManager(mfs);
-    var angularManager = new AngularManager(sourceManager);
+    console.log(process.argv);
 
-    var sourceFiles = recurse('D:/Code/neogov/platform/src/WebApp/scripts/app', s => s.endsWith('.ts') || s.endsWith('.js'));
-    var htmlFiles = recurse('D:/Code/neogov/platform/src/WebApp', s => {
+    var dryRun : boolean;
+    var projectRoot = mpath.resolve('.');
+    if (process.argv.length > 2) {
+        dryRun = process.argv[2] == '--dry';
+    }
 
-        s = mpath.relative('D:/Code/neogov/platform/src/WebApp', s);
-        if (s.endsWith('.cshtml') || s.endsWith('.html')) {
-            if (s.startsWith('Content'))
-                return false;
-            if (s.startsWith('assets'))
-                return false;
-            if (s.startsWith('vendor'))
-                return false;
-            if (s.startsWith('less'))
-                return false;
-            if (s.startsWith('css'))
-                return false;
-            return true;
+    while (projectRoot != '' && !checkProject(projectRoot)) {
+        console.log('Project not found, looking in: ' + projectRoot);
+        var root = mpath.parse(projectRoot).dir;
+        if (root == projectRoot)
+            break;
+        projectRoot = root;
+    }
+
+    if (!checkProject(projectRoot))
+    {
+        console.error('Valid project not found! Please run the app from your PE project root!');
+
+    } else {
+
+        var sourceManager = new SourceFileManager(mfs);
+        var angularManager = new AngularManager(sourceManager);
+
+        var config = {
+            dryRun: dryRun,
+            sourceDir : mpath.join(projectRoot, 'src/WebApp/scripts/app'),
+            htmlDir : mpath.join(projectRoot, 'src/WebApp')
+        };
+
+        console.log('Running with: ');
+        console.log(config);
+
+        var sourceFiles = recurse(config.sourceDir, s => s.endsWith('.ts') || s.endsWith('.js'));
+        var htmlFiles = recurse(config.htmlDir, s => {
+            s = mpath.relative(config.htmlDir, s);
+            if (s.endsWith('.cshtml') || s.endsWith('.html')) {
+                if (s.startsWith('Content'))
+                    return false;
+                if (s.startsWith('assets'))
+                    return false;
+                if (s.startsWith('vendor'))
+                    return false;
+                if (s.startsWith('less'))
+                    return false;
+                if (s.startsWith('css'))
+                    return false;
+                return true;
+            }
+            return false;
+        });
+
+        var endsWithDirective: string[] = [];
+        sourceFiles.forEach(srcFile => {
+
+            if (srcFile.endsWith('.directive.js') ||
+                srcFile.endsWith('.directive.ts')) {
+                endsWithDirective.push(srcFile);
+            }
+
+            angularManager.addFile(srcFile);
+        });
+
+        htmlFiles.forEach(htmlFile => {
+            angularManager.addFile(htmlFile);
+        })
+
+        console.log(`      Directive files: ${endsWithDirective.length}`);
+        console.log(`Directive definitions: ${angularManager.directives.length}`);
+        console.log();
+
+        angularManager.logStatistics();
+
+        let naming = mcore.extractDirectiveNameFromFile.bind(null, 'D:/Code/neogov/platform/src/WebApp/scripts/app');
+
+        angularManager.tidyUp(naming);
+
+        if (!config.dryRun) {
+            sourceManager.flush();
         }
-        return false;
-    });
-
-    var endsWithDirective: string[] = [];
-    sourceFiles.forEach(srcFile => {
-
-        if (srcFile.endsWith('.directive.js') ||
-            srcFile.endsWith('.directive.ts')) {
-            endsWithDirective.push(srcFile);
-        }
-
-        angularManager.addFile(srcFile);
-    });
-
-    htmlFiles.forEach(htmlFile => {
-        angularManager.addFile(htmlFile);
-    })
-
-    console.log(`      Directive files: ${endsWithDirective.length}`);
-    console.log(`Directive definitions: ${angularManager.directives.length}`);
-    console.log();
-
-    angularManager.logStatistics();
-
-    let naming = mcore.extractDirectiveNameFromFile.bind(null, 'D:/Code/neogov/platform/src/WebApp/scripts/app');
-
-    angularManager.tidyUp(naming);
-
-    sourceManager.flush();
+    }
 }
 
 main();
